1a6040f03a78df31d4461dfe6b5a6aa5e73ee244
denisvovaazamat <enesterov580@gmail.com>
2024-11-15 18:26:01 +0500

    изменен файл программы для работы с ним

--- main.cpp
+++ main.cpp
@@ -1,9 +1,43 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
 
 using namespace std;
 
+void Read_File(const vector<string>& file)
+{
+
+}
+
+void Count_File(const vector<string>& file)
+{
+
+}
+
+void Sort_Vector(const vector<string>& file)
+{
+
+}
+
+void Print_Vector(const vector<string>& file)
+{
+
+}
+
+void Write_vectorInFile(const vector<string>& file)
+{
+
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    vector<string> file;
+
+    Read_File(file);
+    Count_File(file);
+    Sort_Vector(file);
+    Print_Vector(file);
+    Write_vectorInFile(file);
+
     return 0;
 }
